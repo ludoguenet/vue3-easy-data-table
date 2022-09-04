6cc1288549cf274bdfc2deac341c6941a992eacd
ludoguenet <ludovicguenet@gmx.com>
2022-09-04 13:55:23 +0200

    Initial commit

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraphs(asText: true),
+            'user_id' => User::factory()->create(),
+        ];
+    }
+}
